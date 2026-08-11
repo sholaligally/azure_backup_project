8d96cb30e70a0ea6a8964c045df168dffa9aaff8
Shola Ligali <sholaligally@19216811.local>
2026-08-10 23:17:46 -0500

    fixed fmt error on backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform { 
+terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstatebackupezlinks1"
     container_name       = "tfstate"
     key                  = "azure-backup-project.tfstate"
-    }
+  }
 }
